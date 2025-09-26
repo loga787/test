124907f401906c1dfa7e3f4202e8b69a41c270e9
loga787 <loga787@mail.ru>
2025-09-26 17:37:58 +0300

    example 3.1

---
+++ 3-1.py
@@ -0,0 +1,9 @@
+a = input('Введите a: ')
+b = input('Введите b: ')
+print(a,b)
+#c = a
+#a = b
+#b = c
+a, b = b, a
+print(a,b)
+
---
+++ main.py
@@ -0,0 +1,4 @@
+name = input('Введите имя: ')
+print('Привет ', name)
+name = input('Введите фамилию: ')
+print('Привет ' + name)
