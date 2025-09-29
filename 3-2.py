675a7d1e971c8c47f8a730c5dcf5b7f3450e16ad
loga787 <loga787@mail.ru>
2025-09-29 12:54:02 +0300

    3.2 and 3.3

---
+++ 3-2.py
@@ -0,0 +1,35 @@
+print (3 + 4)
+print (10 - 5)
+print (3 * 5)
+print (2 ** 333)
+
+a = -5
+b = 7
+c = a + b
+print (c)
+print (a + b)
+
+print ('Привет ', 2)
+
+
+print (6 * 39)
+print (3 ** 5)
+print (-8 / -4)
+print (10 / 2 + 6)
+
+a = input ('введите 1 число: ')
+b = input ('введите 2 число: ')
+c = input ('введите 3 число: ')
+sum = ( int(a) + int (b) + int (c) )
+sub = ( int(a) - int (b) - int (c) )
+multi = ( int(a) * int (b) * int (c) )
+divi = ( int(a) / int (b) / int (c) )
+exponen = ( int(a) ** int (b) ** int (c) )
+print ('Сложение: ', sum )
+print ('Вычитание: ', sub )
+print ('Умножение: ', multi )
+print ('Деление: ', divi )
+print ('Возведение в степень: ', ( int(a) ** int (b) ** int (c) ) )
+
+
+print(65/0)
