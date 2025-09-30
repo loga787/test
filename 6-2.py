e5cfcaa16aa925a598acc940e0c4c22ac20b62fa
loga787 <loga787@mail.ru>
2025-09-30 17:32:26 +0300

    6.2

---
+++ 6-2.py
@@ -0,0 +1,18 @@
+#number = int(input('Введите число: '))
+#total = 0
+#while number != 0:
+#    total += number
+#    number = int(input('Введите число: '))
+#print(total)
+
+#Задача 2. Закономерности
+a = 0
+while a < 98:
+    a +=7
+    print (a )
+
+# Вариант с выводом чисел в одну строку:
+number = 0
+while number < 98:
+    number += 7
+    print(number, end=' ')
