ecb98e213cf5e6846ce320ca7a1fff628ab3a3d2
loga787 <loga787@mail.ru>
2025-10-08 18:37:28 +0300

    module 10 end and 11-2

---
+++ Итог 10 модуля.py
@@ -0,0 +1,28 @@
+# Задача 1. Пирамидка
+# Напишите программу, которая получает на вход количество уровней пирамиды и выводит их на экран, заполняя нечётными числами:
+
+# rows = int(input("Введите высоту пирамиды: "))
+# new_num = 1
+# for line in range(rows):
+#     space_count = rows - line - 1
+#     print('   ' * space_count, end = '')
+#     for number in range(line + 1):
+#         print(new_num, end='    ')
+#         new_num += 2
+#     print()
+
+# Задача 2. Яма
+# Представьте, что вы разрабатываете компьютерную игру с текстовой графикой. Вам поручили создать генератор ландшафта.
+
+# Что нужно сделать
+# Напишите программу, которая получает на вход число N и выводит на экран числа в виде ямы:
+
+depth = int(input("Введите глубину ямы: "))
+for line in range(depth):
+    for left_number in range(depth,depth - line - 1, -1):
+        print(left_number, end='')
+    point_count = 2 * (depth - line - 1)
+    print('.' * point_count, end='')
+    for right_number in range(depth - line, depth + 1):
+        print(right_number, end='')
+    print('')
